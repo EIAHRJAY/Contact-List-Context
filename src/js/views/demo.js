1e3138047df1de681eedd3d4fc6efea7321d4db4
Eduardo Ribeiro <166298206+EIAHRJAY@users.noreply.github.com>
2024-07-02 11:36:20 +0000

    update.3

--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/demo.css";
@@ -8,17 +8,24 @@ import { BsGeoAltFill } from "react-icons/bs";
 import { BsEnvelopeAtFill } from "react-icons/bs";
 
 
-
-
-
-
-
 export const Demo = () => {
 	const { store, actions } = useContext(Context);
+	const [selectedContact, setSelectedContact] = useState(null);
 
 	useEffect(() => {
 		actions.getContacts();
 	}, []);
+
+	const handleDeleteClick = (contactId) => {
+		setSelectedContact(contactId);
+	};
+
+	const handleDeleteConfirm = () => {
+		if (selectedContact) {
+			actions.deleteContact(selectedContact);
+			setSelectedContact(null);
+		}
+	};
 
 	return (
 		<div className="container">
@@ -28,14 +35,11 @@ export const Demo = () => {
 				<ul className="list-group">
 					{store.contacts.map((contact) => (
 						<li key={contact.id} className="list-group-item d-flex justify-content-between">
-							
 							<div className="d-flex">
-									<img src="https://i.pinimg.com/564x/f3/f7/d1/f3f7d1a93907c3892ce16e906929c3ed.jpg " className="rounded-circle " alt="" style={{ width: '100px', height: '100px' }}></img>
-									
-								</div>
+								<img src="https://i.pinimg.com/564x/f3/f7/d1/f3f7d1a93907c3892ce16e906929c3ed.jpg" className="rounded-circle" alt="" style={{ width: '100px', height: '100px' }}></img>
+							</div>
 
-							<div >
-								
+							<div>
 								<p><BsFillPersonFill /> {contact.name}</p>
 								<p><BsFillTelephoneFill /> {contact.phone}</p>
 								<p><BsEnvelopeAtFill /> {contact.email}</p>
@@ -47,35 +51,29 @@ export const Demo = () => {
 									<button className="btn btn-success mr-2 me-2">Edit</button>
 								</Link>
 
-
-								<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-								
-								Delete
+								<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={`#deleteModal-${contact.id}`} onClick={() => handleDeleteClick(contact.id)}>
+									Delete
 								</button>
 
-								
-								<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-								<div className="modal-dialog">
-									<div className="modal-content">
-									<div className="modal-header">
-										<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-									</div>
+								<div className="modal fade" id={`deleteModal-${contact.id}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+									<div className="modal-dialog">
+										<div className="modal-content">
+											{/* <div className="modal-header">
+												<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+											</div> */}
 
-									<div className="modal-body">
-									 <p>Why delete this contact?</p>
-									</div>
+											<div className="modal-body">
+												<p>Why delete this contact?</p>
+											</div>
 
-									<div className="modal-footer">
-										<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
-										<button type="button" className="btn btn-primary"  data-bs-dismiss="modal" onClick={() => actions.deleteContact(contact.id)}>Yes</button>
+											<div className="modal-footer">
+												<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
+												<button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleDeleteConfirm}>Yes</button>
+											</div>
+										</div>
 									</div>
-									</div >
 								</div>
-								</div>
-								
-								
-								
-							</div>	
+							</div>
 						</li>
 					))}
 				</ul>
